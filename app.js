8e718c5e6fc15d23cb719506f055728b19ff2b80
hellomac87 <hellomac87@gmail.com>
2021-08-28 22:59:50 +0900

    doweet , get, post

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ import express from "express";
 const app = express()
 const port = 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+let id = 0;
+const doweets = [];
+
+// middlewares
+app.use(express.json());
+
+app.get('/doweets', (req, res) => {
+    res.status(200).send(doweets);
+});
+
+app.post('/doweets',(req, res) => {
+    const doweet = {...req.body, id: id++};
+    doweets.push(doweet);
+    res.status(201).send(doweet);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
