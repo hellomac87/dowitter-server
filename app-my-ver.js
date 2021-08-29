95211fd63ba82bdead281187b4d5d7a7a4b7c75b
hellomac87 <hellomac87@gmail.com>
2021-08-29 15:28:37 +0900

    WIP : structure setup

--- app-my-ver.js
+++ app-my-ver.js
@@ -1,12 +1,7 @@
+
 import express from "express";
-import cors from 'cors';
 import morgan from 'morgan';
-import helmet from 'helmet';
-import 'express-async-errors';
-
-import doweetsRouter from './router/doweets.js';
-
-const app = express();
+const app = express()
 const port = 8080;
 
 let id = 1;
@@ -14,20 +9,7 @@ const doweets = [];
 
 // middlewares
 app.use(express.json());
-app.use(helmet());
-app.use(cors());
 app.use(morgan('tiny'));
-
-app.use('/doweets', doweetsRouter);
-
-app.use((req, res, next) => {
-    res.sendStatus(404);
-});
-
-app.use((error,req, res, next) => {
-    console.error(error);
-    res.sendStatus(500);
-});
 
 app.get('/doweets', (req, res) => {
     const {username} = req.query;
