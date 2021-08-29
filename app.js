b141dfc1af9fa6cee2ddcfc6d901a6ef23ff8a59
hellomac87 <hellomac87@gmail.com>
2021-08-30 00:15:28 +0900

    트윗 Rest APIs - 프로젝트 셋업 및 구조 만들기💡

--- app.js
+++ app.js
@@ -29,57 +29,6 @@ app.use((error,req, res, next) => {
     res.sendStatus(500);
 });
 
-app.get('/doweets', (req, res) => {
-    const {username} = req.query;
-    const result = [];
-    if(username) {
-        const doweet = doweets.find(doweet => doweet.username === username);
-        res.status(200).send(doweet);
-    }else {
-        res.status(200).send(doweets);
-    }
-    
-});
-
-app.get('/doweets/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const doweet = doweets.find(doweet => doweet.id === id);
-    if(!!doweet)res.status(200).send(doweet);
-    else res.status(404).send('not found!')
-    
-})
-
-app.post('/doweets',(req, res) => {
-    const doweet = {...req.body, id: id++};
-    doweets.push(doweet);
-    res.status(201).send(doweet);
-});
-
-app.put('/doweets/:id',(req,res) => {
-    const id = Number(req.params.id);
-    const {text} = req.body;
-    const index = doweets.findIndex(doweet => doweet.id === id);
-    doweets[index].text = text;
-    if(index === -1){
-        res.status(401).send('not found!');
-        return;
-    }else{
-        res.status(200).send(doweets[index]);
-    }
-});
-
-app.delete('/doweets/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const index = doweets.findIndex(doweet => doweet.id === id);
-    if(index === -1) {
-        res.status(401).send('not found!');
-        return;
-    }else{
-        doweets.splice(index,1);
-        res.status(204).send('success');
-    }
-})
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
--- router/doweets.js
+++ router/doweets.js
@@ -3,4 +3,55 @@ import 'express-async-errors';
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    const {username} = req.query;
+    const result = [];
+    if(username) {
+        const doweet = doweets.find(doweet => doweet.username === username);
+        res.status(200).send(doweet);
+    }else {
+        res.status(200).send(doweets);
+    }
+    
+});
+
+router.get('/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const doweet = doweets.find(doweet => doweet.id === id);
+    if(!!doweet)res.status(200).send(doweet);
+    else res.status(404).send('not found!')
+    
+})
+
+router.post('',(req, res) => {
+    const doweet = {...req.body, id: id++};
+    doweets.push(doweet);
+    res.status(201).send(doweet);
+});
+
+router.put('/:id',(req,res) => {
+    const id = Number(req.params.id);
+    const {text} = req.body;
+    const index = doweets.findIndex(doweet => doweet.id === id);
+    doweets[index].text = text;
+    if(index === -1){
+        res.status(401).send('not found!');
+        return;
+    }else{
+        res.status(200).send(doweets[index]);
+    }
+});
+
+router.delete('/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const index = doweets.findIndex(doweet => doweet.id === id);
+    if(index === -1) {
+        res.status(401).send('not found!');
+        return;
+    }else{
+        doweets.splice(index,1);
+        res.status(204).send('success');
+    }
+})
+
 export default router;
