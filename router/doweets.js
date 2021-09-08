83e36b8d1a743c82c3f989dd519449b20b901f4b
hellomac87 <hellomac87@gmail.com>
2021-09-09 00:06:34 +0900

    validate

--- router/doweets.js
+++ router/doweets.js
@@ -1,19 +1,31 @@
 import express from "express";
 import 'express-async-errors';
+import { body } from 'express-validator';
 
 import * as doweetController from '../controller/doweet.js';
+import { validate } from '../middleware/validator.js';
 
 const router = express.Router();
 
+const validateDoweet = [
+    body('text')
+        .trim()
+        .isLength({min: 3})
+        .withMessage('text should be at least 3 charactors'),
+     validate
+]
 // GET /doweets
 // GET /doweets?username=:username
 router.get('/', doweetController.getDoweets);
 
 router.get('/:id', doweetController.getDoweet);
 
-router.post('/',doweetController.createDoweet);
+router.post(
+    '/',
+    validateDoweet
+    ,doweetController.createDoweet);
 
-router.put('/:id',doweetController.updateDoweet);
+router.put('/:id',validateDoweet,doweetController.updateDoweet);
 
 router.delete('/:id', doweetController.deleteDoweet)
 
